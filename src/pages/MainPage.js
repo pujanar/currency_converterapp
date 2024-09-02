09af52a8c38f75aeb9a81f50a4b5978f27157f3a
pujanar <148024375+pujanar@users.noreply.github.com>
2024-09-02 22:39:06 +0530

    changes

---
+++ src/pages/MainPage.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function MainPage() {
+  return (
+    <div>
+        <h1 className='lg:mx-32 text-5xl font-bold text-yellow-500'>Convert Your Currencies Today</h1>
+        <p className='lg:mx-32 opacity-80 py-6'>The Currency Converter App offers real-time currency conversions with accurate exchange rates. It features a simple interface where users can select their base and target currencies for instant results. Designed for travelers, shoppers, and financial professionals, it ensures users are always updated with global rates. Additionally, the app supports historical data and exchange rate charts for deeper insights.</p>
+
+        <div>
+          <section>
+            <form>
+              <div>
+                <label htmlFor=''>Date</label>
+                <input type='date'/>
+              </div>
+              <div>
+                <label> ` 
+                  
+                </label>
+              </div>
+            </form>
+          </section>
+        </div>
+    </div>
+  )
+}
