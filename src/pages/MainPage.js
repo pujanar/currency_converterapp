b7030049bb7d9b444a84ae82d8b42273e37a6ead
pujanar <148024375+pujanar@users.noreply.github.com>
2024-09-09 00:24:57 +0530

    finalized areas prt2

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -10,10 +10,39 @@ export default function MainPage() {
           <section className='w-full lg:w-1/2'>
             <form>
               
-              <div class="mb-5">
-                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date</label>
-                <input type="date" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+              <div className="mb-4">
+                <label for="date" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date</label>
+                <input type="date" id="date" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-yellow-500" placeholder="name@flowbite.com" required />
               </div>
+
+              <div className="mb-4">
+                <label htmlFor="" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Source Currency</label>
+                <select
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-yellow-500"
+                id=''
+                name=''>
+                  <option value="">Select relevant currency</option>
+                </select>
+                
+              </div>
+
+              <div className="mb-4">
+                <label htmlFor="" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Target Currency </label>
+                <select
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-yellow-500"
+                id=''
+                name=''>
+                  <option value="">Select Target currency</option>
+                </select>
+                
+              </div>
+
+              <div className="mb-4">
+                <label for="amount" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Amount</label>
+                <input type="text" id="date" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-yellow-500" placeholder="" required />
+              </div>
+              <button className='bg-yellow-300 hover:bg-yellow-500 text-black font-extrabold py-2 px-4 rounded-md'>SUBMIT</button>
+              
               
             </form>
           </section>
