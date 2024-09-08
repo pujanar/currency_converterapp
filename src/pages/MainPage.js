f4ddcc2a160ba98c2f20a1c1a28484b96e058608
pujanar <148024375+pujanar@users.noreply.github.com>
2024-09-08 22:55:13 +0530

    added email

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -10,13 +10,17 @@ export default function MainPage() {
           <section>
             <form>
               <div>
-                <label htmlFor=''>Date in the calander</label>
+                <label htmlFor=''>Date</label>
                 <input type='date'/>
               </div>
               <div>
-                <label> ` 
-                  
-                </label>
+              <label> 
+                  <form className="max-w-sm mx-auto">
+                  <div class="mb-5">
+                    <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                      <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+                  </div>
+              </label>
               </div>
             </form>
           </section>
